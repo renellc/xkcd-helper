65eadc5df02ea9b970f97ce63e031af4382e8998
Renell Castro <rcskca@gmail.com>
2017-07-29 03:01:31 -0700

    Added documentation + removed unnecessary comments

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-/* jshint esversion: 6 */
 const https = require("https");
 const hostUrl = "https://www.xkcd.com/";
 const comicPath = "info.0.json";
 
+
+/**
+ * getData: Retrives the JSON data from https://www.xkcd.com/
+ * 
+ * @param {number} [comicNum=null]: If no value is given, function will get the latest comic's data. Else, will get the specified comic number (if it exists). 
+ * @returns: If data is able to be retrieved, the function can be resolved with the data that is received. If there is an error communicating with the servers, the function will reject with the error.
+ */
 let getData = function (comicNum = null) {
     let comicUrl;
     if (comicNum) comicUrl = hostUrl + comicNum + "/" + comicPath;
@@ -24,6 +30,12 @@ let getData = function (comicNum = null) {
     });
 };
 
+
+/**
+ * getLatest: Gets the latest comic that has been released on https://www.xkcd.com/
+ * 
+ * @returns: The latest comic's data.
+ */
 let getLatest = function () {
     return new Promise((resolve, reject) => {
         getData().then(comicData => resolve(comicData))
@@ -31,6 +43,13 @@ let getLatest = function () {
     });
 };
 
+
+/**
+ * getComic: Gets a specific comic's data.
+ * 
+ * @param {number} comicNum: Any integer that is greater than 1. 
+ * @returns: The requested comic's data.
+ */
 let getComic = function (comicNum) {
     return new Promise((resolve, reject) => {
         getData().then(latestComic => {
@@ -49,9 +68,16 @@ let getComic = function (comicNum) {
     });
 };
 
+
+/**
+ * getRandom: Gets a random comic from https://www.xkcd.com/
+ * 
+ * @returns: The random comic's data.
+ */
 let getRandom = function () {
     return new Promise((resolve, reject) => {
         getData().then(latestComic => {
+            // Gets random number between 1 and the latest comic's number
             let randComicNum = Math.floor(Math.random() * latestComic.num);
             getData(randComicNum).then(randComicData => resolve(randComicData))
             .catch(err => reject(err));
