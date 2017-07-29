b9f1c936a65f732037c70e2c2b7e9a7779c4d3d7
Renell Castro <rcskca@gmail.com>
2017-07-29 02:53:11 -0700

    Can now get a specific comic

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let getData = function (comicNum = null) {
     return new Promise((resolve, reject) => {
         https.get(comicUrl, res => {
             let data = "";
-            res.on("data", chunk => rawData += chunk);
+            res.on("data", chunk => data += chunk);
             res.on("end", () => {
                 try {
                     data = JSON.parse(data);
@@ -27,6 +27,24 @@ let getData = function (comicNum = null) {
 let getLatest = function () {
     return new Promise((resolve, reject) => {
         getData().then(comicData => resolve(comicData))
+        .catch(err => reject(err));
+    });
+};
+
+let getComic = function (comicNum) {
+    return new Promise((resolve, reject) => {
+        getData().then(latestComic => {
+            if (comicNum < 1) {
+                let errMsg = "Number must be greater than 1. Please re-enter a valid number.";
+                reject(new Error(errMsg));
+            } else if (comicNum > latestComic.num) {
+                let errMsg = "That comic number is too high! The latest comic number is currently " + latestComic.num;
+                reject(new Error(errMsg));
+            } else {
+                getData(comicNum).then(comicData => resolve(comicData))
+                .catch(err => reject(err));
+            }
+        })
         .catch(err => reject(err));
     });
 };
